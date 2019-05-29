1739e18243a53f5222f0c64a4ac3b078adafe4bc
gulanf <filip.gulan@mavenir.com>
2019-05-29 14:50:16 +0200

    demo example material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import { FormControlComponent } from './form-control/form-control.component';
+import { FormGroupComponent } from './form-group/form-group.component';
+import { FormGroupValidationComponent } from './form-group-validation/form-group-validation.component';
+import { FormGroupBuilderComponent } from './form-group-builder/form-group-builder.component';
+import { DynamicFormGroupComponent } from './dynamic-form-group/dynamic-form-group.component';
+import { DynamicFormArrayComponent } from './dynamic-form-array/dynamic-form-array.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormControlComponent,
+    FormGroupComponent,
+    FormGroupValidationComponent,
+    FormGroupBuilderComponent,
+    DynamicFormGroupComponent,
+    DynamicFormArrayComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule // IMPORTANT
   ],
   providers: [],
   bootstrap: [AppComponent]
